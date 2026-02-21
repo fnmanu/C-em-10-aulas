e0a31001851cca957ff5d2727f7a6da0b94010d7
Emanuela F Neves <164039949+fnmanu@users.noreply.github.com>
2026-02-21 17:09:29 -0300

    Create main.c

---
+++ ex4/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &n);
+    
+    printf("O numero inteiro digitado foi %d", n);
+
+    return 0;
+}
