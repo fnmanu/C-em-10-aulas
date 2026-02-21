1569ee898ec5e57d6562fd19cffea0704f219299
Emanuela F Neves <164039949+fnmanu@users.noreply.github.com>
2026-02-21 16:58:32 -0300

    Add files via upload
    Introdução ao C

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    float real, dolar, cot;
+    printf("digite o valor em real: ");
+    scanf("%f", &real);
+    printf("Digite a cotacao do dolar: ");
+    scanf("%f", &cot);
+    
+    dolar = real/cot;
+    
+    printf("R$%.2f convertido em dólar e US$%.2f", real, dolar);
+
+    return 0;
+}
